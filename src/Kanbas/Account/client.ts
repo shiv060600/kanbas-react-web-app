ad050240cbdd59932b7977476387494e1eb43587
shiv060600 <bhutani.sh@northeastern.edu>
2024-11-22 19:58:37 -0500

    commit

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosWithCredentials = axios.create({ withCredentials: true });
-export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+//export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+const REMOTE_SERVER = "https://kanbase-node-server-app.onrender.com"
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
