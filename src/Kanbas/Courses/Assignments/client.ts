4495f97951d0e765b4f6afe2b0187c996007b641
shiv060600 <bhutani.sh@northeastern.edu>
2024-11-22 19:22:28 -0500

    commit

--- src/Kanbas/Courses/Assignments/client.ts
+++ src/Kanbas/Courses/Assignments/client.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import "dotenv/config";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 const ASSIGNMENTS_API = `${REMOTE_SERVER}/api/assignments`;
